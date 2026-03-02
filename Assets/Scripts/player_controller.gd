559f10ec8abb91b7999d674036d79ca3f74b9195
Fontealba Hinrichsen Alycia <afontealba@univ-lyon2.fr>
2026-03-02 17:17:50 +0100

    point counter system

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -3,10 +3,13 @@ class_name PlayerController
 
 @export var speed = 10.0
 @export var jump_power = 10.0
+@onready var point_label = %Label
 
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
+
+var point_counter = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -26,3 +29,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("object"):
+		set_object(point_counter + 1)
+		print(point_counter)
+
+func set_object(new_object_count: int) -> void:
+	point_counter = new_object_count
+	point_label.text = "Point: " + str(point_counter)
