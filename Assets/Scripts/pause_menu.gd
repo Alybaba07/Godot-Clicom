1db75f1c51e15069f7b299c7f3def7d10dde745c
Alybaba07 <alycia.fontealba@gmail.com>
2026-03-04 13:12:45 +0100

    script pause menu

--- Assets/Scripts/pause_menu.gd
+++ Assets/Scripts/pause_menu.gd
@@ -7,9 +7,19 @@ func pause():
 	get_tree().paused = true
 	
 func escape():
-	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
 		resume()
 	
-		
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Assets/Scenes/main_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _process(delta: float) -> void:
+	escape()
