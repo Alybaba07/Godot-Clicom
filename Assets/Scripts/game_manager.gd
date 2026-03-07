e0f024ab5807b4e0025300877e48c1b5f6c1e1e9
Alybaba07 <alycia.fontealba@gmail.com>
2026-03-07 19:38:03 +0100

    lvl2 completed, sounds and musics completed

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -7,20 +7,22 @@ var area_path = "res://Assets/Scenes/Areas/"
 var object = 0
 var area_container : Node2D
 var player : PlayerController
-
 var music_player : AudioStreamPlayer
+#var hud : HUD
 
 func _ready():
+	#Pour HUD
+	#hud = get_tree().get_first_node_in_group("hud")
+	
+	if music_player == null:
+		music_player = AudioStreamPlayer.new()
+		add_child(music_player)
+		music_player.process_mode = Node.PROCESS_MODE_ALWAYS
+	
 	area_container = get_tree().get_first_node_in_group("area_container")
 	player = get_tree().get_first_node_in_group("player")
-	
-	music_player = AudioStreamPlayer.new()
-	add_child(music_player)
-	
-	if area_container == null:
-		return
-		
-	load_area(starting_area)
+	if area_container != null:
+		load_area(starting_area)
 
 func next_area():
 	current_area += 1
@@ -39,10 +41,12 @@ func load_area(area_number):
 	
 func add_object():
 	await get_tree().process_frame
-	
 	var remaining_objects = get_tree().get_nodes_in_group("object")
+	var objet_restant = get_tree().get_nodes_in_group("object")
 	
-	print("Objets restants : ", remaining_objects.size())
+	# Pour HUD
+	#objet_restant -= 1
+	#hud.update_object_label(objet_restant)
 	
 	if remaining_objects.size() == 0:
 		var portal = get_tree().get_first_node_in_group("area_change") as AreaChange
@@ -55,14 +59,22 @@ func reset_object():
 	object = 0
 	
 func play_menu_music():
-	if music_player.playing:
+	_play_music("res://Assets/Audio/Audio/Menu(shittyjazz).mp3")
+
+func play_level_music():
+	_play_music("res://Assets/Audio/Audio/level.mp3")
+
+# Fonction interne pour éviter les coupures
+func _play_music(track_path: String):
+	# Si la musique joue déjà la BONNE piste, on ne touche à rien (pas de coupure)
+	if music_player.playing and music_player.stream and music_player.stream.resource_path == track_path:
 		return
 		
-	var stream = load("res://Assets/Audio/Audio/Menu(shittyjazz).mp3")
+	var stream = load(track_path)
 	if stream:
 		music_player.stream = stream
 		music_player.play()
-	
+
 func stop_music():
 	music_player.stop()
 	
@@ -72,10 +84,7 @@ func play_sfx(sound_path: String):
 		var audio_player = AudioStreamPlayer.new()
 		add_child(audio_player)
 		audio_player.stream = stream
-		
-		# Ajoute ces deux lignes pour varier un peu la hauteur du son
 		audio_player.pitch_scale = randf_range(0.9, 1.1) 
-		
 		audio_player.play()
 		audio_player.finished.connect(audio_player.queue_free)
 	else:
